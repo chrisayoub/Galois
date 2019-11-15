322ce798057cd41a730100c44c8f47e84bc9d5b0
Chris Ayoub <chris.ayoubtexas@gmail.com>
2019-11-15 14:19:26 -0600

    headers

--- lonestardist/bc/mrbc_tree_cuda.cuh
+++ lonestardist/bc/mrbc_tree_cuda.cuh
@@ -4,11 +4,49 @@ const uint32_t infinity = std::numeric_limits<uint32_t>::max() >> 2;
 class CUDATree {
 	// TODO implement this to be MRBCTree but for CUDA
 public:
-	__device__ void initialize() {
+	__device__
+	void initialize() {
 		// TODO
 	}
 
-	__device__  void setDistance(uint32_t index, uint32_t newDistance) {
+	__device__
+	void setDistance(uint32_t index, uint32_t newDistance) {
+		// TODO
+	}
+
+	__device__
+	uint32_t getIndexToSend(uint32_t roundNumber) {
+		// TODO
+	}
+
+	__device__
+	bool moreWork() {
+		// TODO
+	}
+
+	__device__
+	void markSent(uint32_t roundNumber) {
+		// TODO
+	}
+
+	__device__
+	void setDistance(uint32_t index, uint32_t oldDistance, uint32_t newDistance) {
+		// TODO
+	}
+
+	__device__
+	void prepForBackPhase() {
+		// TODO
+	}
+
+	__device__
+	uint32_t backGetIndexToSend(const uint32_t roundNumber,
+            const uint32_t lastRound) {
+		// TODO
+	}
+
+	__device__
+	bool isZeroReached() {
 		// TODO
 	}
 };
