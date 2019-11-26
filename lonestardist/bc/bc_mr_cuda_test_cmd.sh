7b2153207e8e2a71819bda8fe14e1b86075794e7
Chris Ayoub <chris.ayoubtexas@gmail.com>
2019-11-25 18:56:00 -0600

    script for test

---
+++ lonestardist/bc/bc_mr_cuda_test_cmd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Execute this command from build directory to do a small test on a single GPU
+lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -pset=g -numRoundSources=4096
