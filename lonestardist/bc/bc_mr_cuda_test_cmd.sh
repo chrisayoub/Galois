a6a53e5217304d743699a6ee6b6f5d52dda761db
chris <chris.ayoubtexas@gmail.com>
2019-12-05 14:51:38 -0600

    new cmd

--- lonestardist/bc/bc_mr_cuda_test_cmd.sh
+++ lonestardist/bc/bc_mr_cuda_test_cmd.sh
@@ -9,7 +9,13 @@ make -j bc_mr
 # Execute this command from build directory to do a small test on a single GPU
 lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr -pset=g -numRoundSources=1024
 
-
 # Execute this command if on the server
 lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat10.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat10.tgr -pset=g -numRoundSources=1024
+
+# Bigger graph test, start with 1 source and make larger
+
+lonestardist/bc/bc_mr \
+ /net/ohm/export/iss/inputs/unweighted/withRandomWeights/livejournal.wgr \ 
+ -graphTranspose=/net/ohm/export/iss/inputs/unweighted/withRandomWeights/transpose/livejournal.twgr \
+  -pset=g -numRoundSources=1
 
