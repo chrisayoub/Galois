04a9135c3cc342c798d8946b8a612fbdd7844f1a
Chris Ayoub <chris.ayoubtexas@gmail.com>
2019-12-08 04:23:12 -0600

    updated tester

--- lonestardist/bc/bc_mr_cuda_test_cmd.sh
+++ lonestardist/bc/bc_mr_cuda_test_cmd.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Get inputs
-make input
+# # Get inputs
+# make input
 
-# Build
-make -j bc_mr
+# # Build
+# make -j bc_mr
 
-# Execute this command from build directory to do a small test on a single GPU
-lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr -pset=g -numRoundSources=1024
-lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat15.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat15.tgr -pset=g -numRoundSources=4096 -runs=1
+# # Execute this command from build directory to do a small test on a single GPU
+# lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr -pset=g -numRoundSources=1024
+# lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat15.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat15.tgr -pset=g -numRoundSources=4096 -runs=1
 
-# Execute this command if on the server
-lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat10.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat10.tgr -pset=g -numRoundSources=1024
+# # Execute this command if on the server
+# lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat10.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat10.tgr -pset=g -numRoundSources=1024
 
-lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat15.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat15.tgr -pset=g -numRoundSources=4096
+# lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat15.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat15.tgr -pset=g -numRoundSources=4096
 
-# GPU cmd local
-lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr  -numRoundSources=1024 -runs=10 -pset=g | grep Timer_
+# # GPU cmd local
+# lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr  -numRoundSources=1024 -runs=10 -pset=g | grep Timer_
 
-# CPU cmd local
-lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr  -numRoundSources=1024 -runs=10 -pset=c -t=4 | grep Timer_
+# # CPU cmd local
+# lonestardist/bc/bc_mr inputs/small_inputs/scalefree/rmat10.gr -graphTranspose=inputs/small_inputs/scalefree/transpose/rmat10.tgr  -numRoundSources=1024 -runs=10 -pset=c -t=4 | grep Timer_
 
-# cmake
-cmake ../. -DENABLE_HETERO_GALOIS=1  # -DCMAKE_BUILD_TYPE=Debug 
+# # cmake
+# cmake ../. -DENABLE_HETERO_GALOIS=1  # -DCMAKE_BUILD_TYPE=Debug 
 
 ############################################################
 ############################################################
@@ -34,6 +34,9 @@ lonestardist/bc/bc_level /net/ohm/export/iss/inputs/scalefree/rmat15.gr -graphTr
  
 # Tester bc_mr
 lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat15.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat15.tgr -pset=g -runs=10 -numRoundSources=1024 -statFile=mr.txt > mr_verify.txt
+
+# Tester bc_mr CPU
+lonestardist/bc/bc_mr /net/ohm/export/iss/inputs/scalefree/rmat15.gr -graphTranspose=/net/ohm/export/iss/inputs/scalefree/transpose/rmat15.tgr -pset=c -numRoundSources=4096 -t=8 -runs=10 -statFile=cpu.txt
 
 # Compare times
 grep Timer_ level.txt
