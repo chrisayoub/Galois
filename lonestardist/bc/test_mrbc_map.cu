554f82621aceef6c71b70c3838428272e56c7ea1
Chris Ayoub <chris.ayoubtexas@gmail.com>
2019-12-08 01:16:08 -0600

    fix tester

--- lonestardist/bc/test_mrbc_map.cu
+++ lonestardist/bc/test_mrbc_map.cu
@@ -3,18 +3,19 @@
 #include <stdio.h>
 #include <iostream>
 
+
+
 #include "gg.h"
 #include "galois/cuda/DynamicBitset.h"
+
+#define check_cuda_kernel check_cuda(cudaGetLastError());
+
 #include "mrbc_map_cuda.cuh"
 
 #define DEVICE_ID 0
 
 __global__
-void testMap() {
-	uint32_t NUM_SRC = 10;
-	CUDAMap* map = new CUDAMap(NUM_SRC);
-	printf("Created new CUDAMap!\n");
-
+void testMap(CUDAMap* map) {
 	uint32_t KEY = 3;
 
 	printf("1: Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
@@ -51,7 +52,7 @@ void testMap() {
 	}
 
 	printf("6: Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
-	// Now try resizing with many inserts, ensure no crach
+	// Now try with many inserts, ensure no crash
 	for (uint32_t i = 0; i < 50; i++) {
 		map->get(i);
 	}
@@ -71,17 +72,20 @@ int main(int argc, char** argv) {
 	cudaGetDeviceCount(&devCount);
 	cudaDeviceProp devProp;
 	if (devCount) {
-		cudaSetDevice(DEVICE_ID);  // be changed later
+		cudaSetDevice(DEVICE_ID);
 		cudaGetDeviceProperties(&devProp, DEVICE_ID);
 	}
 	printf("Device: %s\n", devProp.name);
 
 	// Give enough memory
-	size_t heapSpace = 5 * 100000000;
+	size_t heapSpace = 100 * 1024 * 1024;
 	cudaDeviceSetLimit(cudaLimitMallocHeapSize, heapSpace);
 
+	// Get map
+	CUDAMap* map = CUDAMap::getDeviceMap(10, 100);
+
 	// Call kernel that will use map
-	testMap<<<1, 1>>>();
+	testMap<<<1, 1>>>(map);
 
 	cudaDeviceSynchronize();
 
